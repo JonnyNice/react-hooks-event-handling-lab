602d88c0ec5fcd1d274bcb91195d867f27a098be
Ian Holsteen <ianholsteen@gmail.com>
2022-11-23 08:54:09 -0800

    commit tree e416dbab77eb4e2853682e703f159fd7aad54515

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe () {
+    function focusClick(event) {
+        console.log('Good!');
+    }
+    function blurClick() {
+        console.log('Hey! Eyes on me!');
+    }
+    return (
+        <button onFocus={focusClick} onBlur={blurClick}>'Eyes on me'</button>
+    );
+}
+
+export default EyesOnMe;
